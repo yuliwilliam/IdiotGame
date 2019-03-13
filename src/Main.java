2a1dd5fe331c565d02d96d428d7fddf95525ba37
Lizhi Yu <leedsyu@yahoo.com>
2019-03-12 21:38:17 -0400

    prevent button stay outiside the frame, limit min frame size

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,14 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
+
+        //add questions
         LinkedList questions = new LinkedList();
-        questions.add("Are you Idiot？");
-        questions.add("Want to be happier？");
+        questions.add("Are you Idiot?");
+        questions.add("Want to be happier?");
 
 
+        //init JFrame size
         int height = 500;
         int width = (int) (height * 0.618);
 
@@ -20,7 +23,6 @@ public class Main {
         JButton noBtn = new JButton("NO");
 
         panel.setLayout(null);
-        System.out.println(frame.getWidth());
 
         Dimension labelSize = label.getPreferredSize();
         Dimension btnSize = yesBtn.getPreferredSize();
@@ -36,16 +38,13 @@ public class Main {
             if (questions.size() == 0) {
                 System.exit(0);
             }
+
+            //set label to new question
             label.setText(questions.pop().toString());
             Dimension newLabelSize = label.getPreferredSize();
             label.setBounds(frame.getWidth() / 2 - (int) newLabelSize.getWidth() / 2, 100, (int) newLabelSize.getWidth(), (int) newLabelSize.getHeight());
 
         });
-
-        panel.add(label);
-        panel.add(yesBtn);
-        panel.add(noBtn);
-
 
         frame.addWindowListener(new java.awt.event.WindowAdapter() {
             @Override
@@ -55,11 +54,17 @@ public class Main {
             }
         });
 
+        panel.add(label);
+        panel.add(yesBtn);
+        panel.add(noBtn);
+
         frame.pack();
         frame.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
         frame.setSize(new Dimension(width, height));
         frame.setVisible(true);
 
+
+        //change  position of button when mouse is on it
         while (true) {
 
             Point frameLocation = frame.getLocation();
@@ -69,18 +74,27 @@ public class Main {
             double x = mouseLocation.getX() - frameLocation.getX();
             double y = mouseLocation.getY() - frameLocation.getY() - frame.getInsets().top;
 
+            //limit the minimum JFrame size
+            if (frame.getHeight() < height || frame.getWidth() < width){
+                frame.setSize(new Dimension(width, height));
+            }
+
+            //random change location
             if (noBtn.getBounds().contains(x, y)) {
                 Random rand = new Random();
-                noBtn.setBounds(rand.nextInt(frame.getWidth() - (int) btnSize.getWidth()), rand.nextInt(frame.getHeight() - (int) btnSize.getHeight()), (int) btnSize.getWidth(), (int) btnSize.getHeight());
-
+                noBtn.setBounds(rand.nextInt(frame.getWidth() - (int) btnSize.getWidth()), rand.nextInt(frame.getHeight() - (int) btnSize.getHeight() - frame.getInsets().top), (int) btnSize.getWidth(), (int) btnSize.getHeight());
             }
+
+            //random change location if outside frame
+            if(noBtn.getX() > frame.getWidth() - (int) btnSize.getWidth() || noBtn.getY() >= frame.getHeight() - (int) btnSize.getHeight() - frame.getInsets().top){
+                Random rand = new Random();
+                noBtn.setBounds(rand.nextInt(frame.getWidth() - (int) btnSize.getWidth()), rand.nextInt(frame.getHeight() - (int) btnSize.getHeight() - frame.getInsets().top), (int) btnSize.getWidth(), (int) btnSize.getHeight());
+            }
+
             labelSize = label.getPreferredSize();
             label.setBounds(frame.getWidth() / 2 - (int) labelSize.getWidth() / 2, 100, (int) labelSize.getWidth(), (int) labelSize.getHeight());
             yesBtn.setBounds(50, frame.getHeight() - 100, (int) btnSize.getWidth(), (int) btnSize.getHeight());
 
         }
-
     }
-
-
 }
